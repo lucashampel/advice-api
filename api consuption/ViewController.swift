58d573d4ca6b47a0a968f944e76840b8f0c69c39
lucas <lucashampell@gmail.com>
2022-12-16 01:14:42 -0300

    Initial Commit

---
+++ api consuption/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  api consuption
+//
+//  Created by lucas meireles on 16/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
